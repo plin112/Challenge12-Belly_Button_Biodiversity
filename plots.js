576291fa3c79054c9e1f28268aa213a7032dd16f
Admin <admin@Monopolys-MacBook-Pro.local>
2020-09-29 23:25:03 -0400

    uploading challenges

--- plots.js
+++ plots.js
@@ -43,28 +43,78 @@ function init() {
 })}
 
 function optionChanged(newSample) {
-  console.log(newSample);
-}
-
-function optionChanged(newSample) {
   buildMetadata(newSample);
   buildCharts(newSample);
 }
+
 function buildMetadata(sample) {
   d3.json("samples.json").then((data) => {
     var metadata = data.metadata;
     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
     var result = resultArray[0];
-    var PANEL = d3.select("#sample-metadata");
+    var Panel = d3.select("#sample-metadata");
+    Panel.html("");
 
-    PANEL.html("");
-    PANEL.append("h6").text(result.id);
-    PANEL.append("h6").text(result.ethnicity);
-    PANEL.append("h6").text(result.gender);
-    PANEL.append("h6").text(result.age);
-    PANEL.append("h6").text(result.location);
-    PANEL.append("h6").text(result.bbtype);
-    PANEL.append("h6").text(result.wfreq);
+    Object.entries(result).forEach(([key, value]) => {
+      Panel.append("h6").text(`${key}: ${value}`)
+    })
+    
+  });
+} 
+
+function buildCharts(sample) {    
+  d3.json("samples.json").then((data) => {
+    var samples = data.samples;
+    var filterArray = samples.filter(sampleObject => sampleObject.id == sample);
+    var result = filterArray[0];
+    var sample_values = result.sample_values;
+    var otu_ids = result.otu_ids;
+    var otu_labels = result.otu_labels;   
+  
+    // Bar Chart
+    var trace2 = {
+      x: sample_values.slice(0,10).reverse(),
+      y: otu_ids.slice(0,10).map(otuID => `OTU ${otuID}`).reverse(),
+      text: otu_labels.slice(0,10).reverse(),
+      name: "Greek",
+      type: "bar",
+      orientation: "h"
+    };
+    
+    var data = [trace2];
+
+    var layout = {
+      title: "To10 OTUs found in " +sample,
+      margin: {l: 100, r: 100, t: 100, b: 100}
+    };
+
+    Plotly.newPlot("bar", data, layout);  
+  
+
+    // Bubble Chart
+    var trace3 = {
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      mode: 'markers',
+      marker: {
+      size: sample_values,
+      color: otu_ids,
+      colorscale:"Electric"
+      }
+    };
+
+    var data = [trace3];
+
+    var layout = {
+      title: 'Bacteria Cultures per Sample',
+      showlegend: false,
+      hovermode: 'closest',
+      xaxis: {title:"OTU ID: " +sample},
+      margin: {t:30}
+    };
+    
+    Plotly.newPlot('bubble', data, layout); 
   });
 }
 
